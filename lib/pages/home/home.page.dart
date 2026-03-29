16d5b28c22ae66380f1e5d714642559f7c4fa329
Jhonny Araujo <jhonny.a.santana2002@gmail.com>
2026-03-29 15:16:10 -0300

    style: estiliza os widget da página HomePage

--- lib/pages/home/home.page.dart
+++ lib/pages/home/home.page.dart
@@ -1,3 +1,4 @@
+import 'package:app_rick_and_morty/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,24 +7,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text("Rick and Morty"),
-              const Text("Procure pelo personagem"),
-              const TextField(
-                decoration: InputDecoration(
-                  hint: Text("Nome ou identificador"),
-                  prefix: Icon(Icons.search),
+              const Text(
+                "Rick and Morty",
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 44,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
+              const SizedBox(height: 12),
+              const Text(
+                "Procure pelo seu personagem",
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 12),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: primaryColor,
+                  hint: const Text(
+                    "Nome ou identificador",
+                    style: TextStyle(color: Colors.black45),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.search, color: Colors.black45),
+                ),
+              ),
+              const SizedBox(height: 12),
               Row(
                 children: [
                   const Spacer(),
-                  IconButton(onPressed: () {}, icon: const Icon(Icons.list)),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.list, color: primaryColor),
+                  ),
                 ],
               ),
               Expanded(
