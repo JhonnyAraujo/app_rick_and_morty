a823f498d00e4aca0ad4126632b687656f16940a
Jhonny Araujo <jhonny.a.santana2002@gmail.com>
2026-04-07 18:18:21 -0300

    refactor: prepara Personagem para receber cache de cores

--- lib/models/personagem.model.dart
+++ lib/models/personagem.model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Personagem {
   final int id;
   final String name;
@@ -5,6 +7,7 @@ class Personagem {
   final String species;
   final String gender;
   final String imageUrl;
+  final Color color;
 
   Personagem({
     required this.id,
@@ -13,6 +16,7 @@ class Personagem {
     required this.species,
     required this.gender,
     required this.imageUrl,
+    this.color = Colors.white,
   });
 
   factory Personagem.fromMap(Map<String, dynamic> map) {
@@ -23,6 +27,26 @@ class Personagem {
       species: map["species"],
       gender: map["gender"],
       imageUrl: map["image"],
+    );
+  }
+
+  Personagem copyWith({
+    int? id,
+    String? name,
+    String? status,
+    String? species,
+    String? gender,
+    String? imageUrl,
+    Color? color,
+  }) {
+    return Personagem(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      status: status ?? this.status,
+      species: species ?? this.species,
+      gender: gender ?? this.gender,
+      imageUrl: imageUrl ?? this.imageUrl,
+      color: color ?? this.color,
     );
   }
 }
